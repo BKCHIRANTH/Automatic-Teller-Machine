d34acfb957d42dda3fafcfc17c3f1a38a67aaa58
CHIRANTH B K <36081391+BKCHIRANTH@users.noreply.github.com>
2021-03-06 11:09:14 +0530

    Add files via upload
    An illustration of Automatic Teller Machine using concepts of OOPS with Java. Tried to implement assorted operations of any real time ATM machines known.

---
+++ java1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Java1{
+public static void main(String[]args){
+System.out.println("enter two operands");
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=a+b;
+System.out.println("sum="+c);
+}}
---
+++ one.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Example{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the operands");
+int a=scan.nextInt();
+int b=scan.nextInt();
+int r=a+b;
+System.out.println("the sum is="+r);
+}}
